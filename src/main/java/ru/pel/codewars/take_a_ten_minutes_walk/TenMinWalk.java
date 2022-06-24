e9cf8a7452788e6a0186096dbcaa46de412a32fe
lenovo <none@none.org>
2022-06-24 11:07:04 +0300

    Решено

--- src/main/java/ru/pel/codewars/take_a_ten_minutes_walk/TenMinWalk.java
+++ src/main/java/ru/pel/codewars/take_a_ten_minutes_walk/TenMinWalk.java
@@ -1,9 +1,21 @@
 package ru.pel.codewars.take_a_ten_minutes_walk;
 
 public class TenMinWalk {
-    public static boolean isValid(char[] walk){
-        if (walk.length!=10)
+    public static boolean isValid(char[] walk) {
+        if (walk.length != 10)
             return false;
-        return true;
+        int horizontal = 0;
+        int vertical = 0;
+        for (char c : walk) {
+            switch (c) {
+                case 'n' -> vertical++;
+                case 's' -> vertical--;
+                case 'e' -> horizontal++;
+                case 'w' -> horizontal--;
+                default -> throw new IllegalArgumentException("Неизвестное направление");
+            }
+        }
+
+        return horizontal == 0 && vertical == 0;
     }
 }
