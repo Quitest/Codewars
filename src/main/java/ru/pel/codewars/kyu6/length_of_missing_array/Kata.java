31706d8cfdbcb90de0448ce769c697992d792df1
Quitest <Quitest>
2022-06-24 21:24:50 +0300

    Локально тесты проходит успешно. Онлайн - крашится с NPE.

--- src/main/java/ru/pel/codewars/kyu6/length_of_missing_array/Kata.java
+++ src/main/java/ru/pel/codewars/kyu6/length_of_missing_array/Kata.java
@@ -1,9 +1,35 @@
 package ru.pel.codewars.kyu6.length_of_missing_array;
 
-public class Kata
-{
-    public static int getLengthOfMissingArray(Object[][] arrayOfArrays)
-    {
-        return 0;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Kata {
+    private Kata() {
+    }
+
+    public static int getLengthOfMissingArray(Object[][] arrayOfArrays) {
+        if (arrayOfArrays == null || arrayOfArrays.length == 0) {
+            return 0;
+        }
+
+
+
+        int minLength = Arrays.stream(arrayOfArrays)
+                .mapToInt(a -> a.length).min().getAsInt();//FIXME при онлайн тестах, предположительно тут вылетает NPE
+        if (minLength == 0){
+            return 0;
+        }
+        AtomicInteger lengthOfPrevArr = new AtomicInteger(minLength);
+
+        return Arrays.stream(arrayOfArrays)
+                .sorted(Comparator.comparing(arr -> arr.length, Comparator.naturalOrder()))
+                .dropWhile(currArr -> {
+                    boolean result = (currArr.length - lengthOfPrevArr.get()) < 2;
+                    lengthOfPrevArr.set(currArr.length);
+                    return result;
+                })
+                .findFirst().orElseThrow().length-1;
     }
 }
