82cb8f1d691960c295c7f2654a6857725e5dae61
Quitest <Quitest>
2022-06-24 16:59:26 +0300

    Init

---
+++ src/main/java/ru/pel/codewars/kyu6/length_of_missing_array/Kata.java
@@ -0,0 +1,9 @@
+package ru.pel.codewars.kyu6.length_of_missing_array;
+
+public class Kata
+{
+    public static int getLengthOfMissingArray(Object[][] arrayOfArrays)
+    {
+        return 0;
+    }
+}
