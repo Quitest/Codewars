073e1eb930601b3c536417ae9384d3f64fef968c
lenovo <none@none.org>
2022-06-24 13:14:08 +0300

    Решено. Немного другая реализация.

--- src/main/java/ru/pel/codewars/find_the_missing_letter/Kata.java
+++ src/main/java/ru/pel/codewars/find_the_missing_letter/Kata.java
@@ -6,7 +6,7 @@ public class Kata {
      * @param array массив символов, в котором происходит поиск пропущенного символа.
      * @return пропущенный символ. Если пропущенных символов нет, то символ пробела " ".
      */
-    public static char findMissingLetter(char[] array) {
+    public static char findMissingLetter_int(char[] array) {
         int prev = array[0];
         int curr;
         for (int i = 1; i < array.length; i++) {
@@ -14,6 +14,22 @@ public class Kata {
             if (curr - prev > 1)
                 return (char) (curr - 1);
             prev = curr;
+        }
+        return ' ';
+    }
+
+    /**
+     * Метод поиска пропущенного символа. Арифметика делается с типом char (без явных преобразований).
+     * @param array массив символов, в котором происходит поиск пропущенного символа.
+     * @return пропущенный символ. Если пропущенных символов нет, то символ пробела " ".
+     */
+    public static char findMissingLetter(char[] array) {
+        char prevChar = array[0];
+        for (char currChar : array) {
+            if (currChar - prevChar > 1) {
+                return --currChar;
+            }
+            prevChar = currChar;
         }
         return ' ';
     }
