82a78c87be9e064ff45ece4237e940fd1cc4978f
Quitest <Quitest>
2022-06-25 22:14:43 +0300

    Добавлена вторая реализация. Более простая и понятная.

--- src/main/java/ru/pel/codewars/kyu6/enough_is_enough/EnoughIsEnough.java
+++ src/main/java/ru/pel/codewars/kyu6/enough_is_enough/EnoughIsEnough.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class EnoughIsEnough {
     private EnoughIsEnough() {//скрываем публичный конструктор
@@ -19,5 +20,12 @@ public class EnoughIsEnough {
             }
         }
         return array.stream().mapToInt(i -> i).toArray();
+    }
+
+    public static int[] deleteNthVer2(int[] elements, int maxOccurrences){
+        Map<Integer,Integer> occurrences = new HashMap<>();
+        return IntStream.of(elements)
+                .filter(element -> occurrences.merge(element,1, Integer::sum) <= maxOccurrences)
+                .toArray();
     }
 }
