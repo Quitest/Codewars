176c57cc9371e1b68356181be6aaff393e7a8f5f
Quitest <Quitest>
2022-06-25 22:06:26 +0300

    Решено

--- src/main/java/ru/pel/codewars/kyu6/enough_is_enough/EnoughIsEnough.java
+++ src/main/java/ru/pel/codewars/kyu6/enough_is_enough/EnoughIsEnough.java
@@ -1,8 +1,23 @@
 package ru.pel.codewars.kyu6.enough_is_enough;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class EnoughIsEnough {
+    private EnoughIsEnough() {//скрываем публичный конструктор
+    }
+
     public static int[] deleteNth(int[] elements, int maxOccurrences) {
-        //Code here ;)
-        return null;
+        List<Integer> array = new ArrayList<>();
+        Map<Integer, Integer> counter = new HashMap<>();
+        for (int i : elements) {
+            counter.compute(i, (k, v) -> (v == null) ? 0 : ++v);
+            if (counter.get(i) < maxOccurrences) {
+                array.add(i);
+            }
+        }
+        return array.stream().mapToInt(i -> i).toArray();
     }
 }
