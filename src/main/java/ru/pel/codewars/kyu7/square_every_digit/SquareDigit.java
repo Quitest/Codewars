43fc4ee3b7bc140f5ba633e429b2adc8bbf1877a
Quitest <Quitest>
2022-06-25 23:01:18 +0300

    Решено

--- src/main/java/ru/pel/codewars/kyu7/square_every_digit/SquareDigit.java
+++ src/main/java/ru/pel/codewars/kyu7/square_every_digit/SquareDigit.java
@@ -2,6 +2,14 @@ package ru.pel.codewars.kyu7.square_every_digit;
 
 public class SquareDigit {
     public int squareDigits(int n) {
-        // TODO Implement me
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        StringBuilder str = new StringBuilder();
+        while (n > 0) {
+            str.insert(0, (n % 10) * (n % 10)); //Что бы не использовать Math.pow() с последующим преобразованием типов.
+            n /= 10;
+        }
+        return Integer.parseInt(str.toString());
     }
 }
--- src/test/java/ru/pel/codewars/kyu7/square_every_digit/SquareDigitTest.java
+++ src/test/java/ru/pel/codewars/kyu7/square_every_digit/SquareDigitTest.java
@@ -6,8 +6,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SquareDigitTest {
     @Test
-    public void test() {
+    void test() {
         assertEquals(811181, new SquareDigit().squareDigits(9119));
         assertEquals(0, new SquareDigit().squareDigits(0));
+    }
+
+    @Test
+    void test2(){
+        assertEquals(9414, new SquareDigit().squareDigits(3212) );
+    }
+    @Test
+    void test3(){
+        assertEquals(3625490, new SquareDigit().squareDigits(6570) );
     }
 }
