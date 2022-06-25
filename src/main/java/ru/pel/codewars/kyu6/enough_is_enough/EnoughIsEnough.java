fa0b4e54f3fa3cfdd61f4e4988f60758194ce924
Quitest <Quitest>
2022-06-25 19:24:59 +0300

    Init

---
+++ src/main/java/ru/pel/codewars/kyu6/enough_is_enough/EnoughIsEnough.java
@@ -0,0 +1,8 @@
+package ru.pel.codewars.kyu6.enough_is_enough;
+
+public class EnoughIsEnough {
+    public static int[] deleteNth(int[] elements, int maxOccurrences) {
+        //Code here ;)
+        return null;
+    }
+}
