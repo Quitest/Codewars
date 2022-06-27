53872a271eb02502f421dc8392777d12d8def733
lenovo <none@none.org>
2022-06-27 12:04:32 +0300

    init

---
+++ src/main/java/ru/pel/codewars/kyu4/next_bigger_num/Kata.java
@@ -0,0 +1,10 @@
+package ru.pel.codewars.kyu4.next_bigger_num;
+
+public class Kata {
+    public static long nextBiggerNumber(long n)
+    {
+        return n;
+    }
+
+    private Kata(){}
+}
---
+++ src/test/java/ru/pel/codewars/kyu4/next_bigger_num/KataTest.java
@@ -0,0 +1,17 @@
+package ru.pel.codewars.kyu4.next_bigger_num;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KataTest {
+    @Test
+    public void basicTests() {
+        assertEquals(21, Kata.nextBiggerNumber(12));
+        assertEquals(531, Kata.nextBiggerNumber(513));
+        assertEquals(2071, Kata.nextBiggerNumber(2017));
+        assertEquals(441, Kata.nextBiggerNumber(414));
+        assertEquals(414, Kata.nextBiggerNumber(144));
+        assertEquals(19009, Kata.nextBiggerNumber(10990));
+    }
+}
