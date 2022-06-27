d8f970304f81f07daa979ca0040a3bb4c04b77bc
lenovo <none@none.org>
2022-06-27 16:22:40 +0300

    Офлайн тесты пройдены.

--- src/main/java/ru/pel/codewars/kyu4/next_bigger_num/Kata.java
+++ src/main/java/ru/pel/codewars/kyu4/next_bigger_num/Kata.java
@@ -1,7 +1,6 @@
 package ru.pel.codewars.kyu4.next_bigger_num;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,29 +18,31 @@ public class Kata {
 //        int mod = Integer.MIN_VALUE;
         while (n != 0) {
             int digit = (int) n % 10;
-            digitSentence.add(0,digit); //с точки зрения производительности решение так себе.
+            digitSentence.add(0, digit); //с точки зрения производительности решение так себе.
             n = n / 10;
         }
-
-        for (int i = digitSentence.size()-1; i >=0; i--) {
+        List<Integer> leftSubList = new ArrayList<>();
+        for (int i = digitSentence.size() - 1; i >= 0; i--) {
             int curr = digitSentence.get(i);
             int next = digitSentence.get(i - 1);
             if (curr > next) {
-                digitSentence.set(i,next);
-                digitSentence.set(i-1, curr);
-                List<Integer> subList = digitSentence.subList(i, digitSentence.size() - 1);
-                subList.sort(Comparator.reverseOrder());
-//                digitSentence.re
+                digitSentence.set(i, next);
+                digitSentence.set(i - 1, curr);
+
+                List<Integer> rightSubList = digitSentence.subList(i, digitSentence.size());
+                rightSubList.sort(Comparator.naturalOrder());
+                leftSubList = digitSentence.subList(0, i);
+                leftSubList.addAll(rightSubList);
                 break;
             }
         }
-        long nextBigger = Long.parseLong(digitSentence.stream().map(i->i+"").collect(Collectors.joining()));
+        long nextBigger = toLong(leftSubList);
 
-        return nextBigger==src ? -1: nextBigger;
+        return nextBigger == src ? -1 : nextBigger;
     }
 
-    private boolean isBigger(int a, int b) {
-        return a > b;
+    private static long toLong(List<? extends Number> list){
+        return Long.parseLong(list.stream().map(i -> i + "").collect(Collectors.joining()));
     }
 
     private Kata() {
